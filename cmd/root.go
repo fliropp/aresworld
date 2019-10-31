3097c2b05363a6ead6b859c3831c5cb22963fdd7
Are Grottum <grottum@gmail.com>
2019-10-31 15:03:06 +0100

    bogus from a to z

--- cmd/root.go
+++ cmd/root.go
@@ -5,41 +5,42 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
 
-var RootCmd = &cobra.Command{
-	Use:   "",
-	Short: "Ares World",
+var rootCmd = &cobra.Command{
+	Use: "root",
+}
+
+var firstCmd = &cobra.Command{
+	Use:   "are1",
+	Short: "Ares World cmd 1",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(args[0])
+	},
+}
+
+var secondCmd = &cobra.Command{
+	Use:   "are2",
+	Short: "Ares World cmd 2",
+	Run: func(cmd *cobra.Command, args []string) {
+		for _, b := range args {
+			fmt.Println(b)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the RootCmd.
 func Execute() {
-	if err := RootCmd.Execute(); err != nil {
+	if err := firstCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/aresworld.yaml)")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		//fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+	rootCmd.AddCommand(firstCmd)
+	rootCmd.AddCommand(secondCmd)
 }
