d2993d55e7ce74e6a2aaf5b5fb52ac7fc10fbee2
fliropp <grottum@gmail.com>
2019-11-01 07:54:50 +0100

    stupid ass code

--- cmd/root.go
+++ cmd/root.go
@@ -4,24 +4,17 @@ import (
 	"fmt"
 	"os"
 
+  "github.com/fliropp/aresworld/cmd/server"
 	"github.com/spf13/cobra"
 )
 
 var cfgFile string
 
-var rootCmd = &cobra.Command{
+var RootCmd = &cobra.Command{
 	Use: "root",
 }
 
-var firstCmd = &cobra.Command{
-	Use:   "are1",
-	Short: "Ares World cmd 1",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(args[0])
-	},
-}
-
-var secondCmd = &cobra.Command{
+var TestCmd = &cobra.Command{
 	Use:   "are2",
 	Short: "Ares World cmd 2",
 	Run: func(cmd *cobra.Command, args []string) {
@@ -34,13 +27,13 @@ var secondCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the RootCmd.
 func Execute() {
-	if err := firstCmd.Execute(); err != nil {
+	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 }
 
 func init() {
-	rootCmd.AddCommand(firstCmd)
-	rootCmd.AddCommand(secondCmd)
+	RootCmd.AddCommand(TestCmd)
+	RootCmd.AddCommand(server.ServerCmd)
 }
